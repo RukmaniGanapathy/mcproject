4d4640de1adaedbfe1cb0b847382fa7aee13b56c
Rukmani Ganapathy <rukmani.g@asu.edu>
2017-03-30 19:17:05 -0700

    Added model for user, basic structure for the service and an enum to keep track of messages

---
+++ app/src/main/java/edu/project/mobilecomputing/mc_project/enums/MessageEnum.java
@@ -0,0 +1,12 @@
+package edu.project.mobilecomputing.mc_project.enums;
+
+/**
+ * Created by Rukmani on 3/30/17.
+ */
+public class MessageEnum {
+
+    enum Messages{
+        NOTIFY_NEARBY_SUPERMARKET, NOTIFY_FRIENDS_ABOUT_ARRIVAL_TO_GROCERY_STORE, NOTIFY_ITEMS_TO_BUY, ACCEPT_TO_BUY_ITEMS_FOR_FRIEND,
+        REJECT_TO_BUY_ITEMS_FOR_FRIEND
+    }
+}
---
+++ app/src/main/java/edu/project/mobilecomputing/mc_project/service/ApplicationService.java
@@ -0,0 +1,43 @@
+package edu.project.mobilecomputing.mc_project.service;
+
+import java.util.List;
+
+import edu.project.mobilecomputing.mc_project.model.User;
+
+/**
+ * Created by Rukmani on 3/30/17.
+ */
+public abstract class ApplicationService {
+
+    public abstract List<User> getUsers();
+
+    public abstract List<User> getFriends(String userId);
+
+    public abstract void addFriend(User user1, User user2);
+
+    public abstract void removeFriend(User user1, User user2);
+
+    public abstract String getGroceryList(String userId);
+
+    public abstract void deleteUserAccount(String userId);
+
+    public abstract String generateMessages(int useCase);
+
+    //write the logic to send a particular message to a user
+    public abstract void sendMessage(String userId, int useCase);
+
+    public abstract void respondToNotification(String userId, int useCase);
+
+    public abstract boolean areFriendsOnSplitwise(User currentUser, User friend);
+
+    public abstract void makeFriendsOnSplitwise(User currentUser, User friend);
+
+    public abstract void createTransactionOnSplitwise(User buyer, User receiver, String caption, Float amount);
+
+    //TODO add method signatures for accelerometer and Google maps related functionalities
+
+
+
+
+
+}
