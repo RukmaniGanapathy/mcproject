c616b3882fbf4ffe30041781648ad29bc2b819cd
ayse <aozdemir2011@gmail.com>
2017-04-18 19:40:47 -0700

    Login,Register Pages and Firebase connection setting added.

--- app/src/main/java/edu/project/mobilecomputing/mc_project/ui/LoginActivity.java
+++ app/src/main/java/edu/project/mobilecomputing/mc_project/ui/LoginActivity.java
@@ -1,8 +1,78 @@
 package edu.project.mobilecomputing.mc_project.ui;
 
-/**
- * Created by ayse on 4/18/2017.
- */
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import edu.project.mobilecomputing.mc_project.R;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private String email, password;
+    Button btnRegister, btnLogin;
+    EditText editTextEmail, editTextPassword;
+    ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        editTextEmail = ((EditText) findViewById(R.id.email));
+        editTextPassword = ((EditText) findViewById(R.id.password));
+        progressDialog = new ProgressDialog(this);
+        btnLogin = (Button) findViewById(R.id.login);
+        btnLogin.setOnClickListener(this);
+        btnRegister = (Button) findViewById(R.id.register);
+        btnRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnLogin){
+            validateUser();
+        }else if(v == btnRegister)
+        {
+            //redirect to the register page
+            Intent intent = new Intent(v.getContext(), RegisterActivity.class);
+            startActivityForResult(intent, 0);
+        }
+    }
+
+    private boolean validateUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                            //redirect page to the welcome/profile screen
+                            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                            startActivityForResult(intent, 0);
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, "There is a problem, try again...", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        return false;
+    }
 }
--- app/src/main/java/edu/project/mobilecomputing/mc_project/ui/RegisterActivity.java
+++ app/src/main/java/edu/project/mobilecomputing/mc_project/ui/RegisterActivity.java
@@ -1,8 +1,121 @@
 package edu.project.mobilecomputing.mc_project.ui;
 
-/**
- * Created by ayse on 4/18/2017.
- */
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity {
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import edu.project.mobilecomputing.mc_project.R;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText editTextName, editTextEmail, editTextPassword, editTextConfirmPassword;
+    Button btnRegister;
+    private FirebaseAuth firebaseAuth;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        editTextName = ((EditText) findViewById(R.id.name));
+        editTextEmail = ((EditText) findViewById(R.id.email));
+        editTextPassword = ((EditText) findViewById(R.id.password));
+        editTextConfirmPassword = ((EditText) findViewById(R.id.confirmPassword));
+
+        btnRegister = (Button) findViewById(R.id.register);
+        btnRegister.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnRegister )
+        {
+            addUser();
+        }
+    }
+
+    private void addUser(){
+        String name = editTextName.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String password2 = editTextConfirmPassword.getText().toString().trim();
+
+        //progressDialog.setMessage("Registering user...");
+        //progressDialog.show();
+
+        if(checkPassword(password, password2)) {
+            if (!TextUtils.isEmpty(name)) {
+                //String id = dbUsers.push().getKey();
+                //User user = new User(id, name, email, password);
+                //dbUsers.child(id).setValue(user);
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<com.google.firebase.auth.AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<com.google.firebase.auth.AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(RegisterActivity.this, "Register successful..", Toast.LENGTH_SHORT).show();
+                                }
+                                else{
+                                    System.out.println(task.getException().toString());
+                                    System.out.println(task.getResult().toString());
+
+                                    Toast.makeText(RegisterActivity.this, "There is a problem, try again..", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            } else {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "Passwords entered doesn't match", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean checkPassword(String psw1, String psw2){
+        //both are equal
+        //check length
+        int length = psw1.length();
+
+        if(length >= 5) {
+            if (psw1.equals(psw2)) {
+                return true;
+            } else {
+                return false;
+            }
+        }else{
+            Toast.makeText(this, "Password should be at least 6 character", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    private void encryptPassword(){
+        //encrypt password
+    }
+
 }
