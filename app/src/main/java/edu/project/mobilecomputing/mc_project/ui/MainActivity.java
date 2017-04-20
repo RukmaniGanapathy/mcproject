01aafc18913b652d86b0279e44614cf2b39e6c3e
Rukmani Ganapathy <rukmani.g@asu.edu>
2017-04-20 01:21:27 -0700

    implemented delete account/ log out functionality

--- app/src/main/java/edu/project/mobilecomputing/mc_project/ui/MainActivity.java
+++ app/src/main/java/edu/project/mobilecomputing/mc_project/ui/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.project.mobilecomputing.mc_project.ui;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -13,6 +15,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 import edu.project.mobilecomputing.mc_project.R;
 
@@ -74,16 +82,43 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (id == R.id.action_deleteaccount) {
-            Toast.makeText(getApplicationContext(),"Delete Account",Toast.LENGTH_LONG).show();
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            FirebaseDatabase.getInstance().getReference().child("users").child(user.getUid()).removeValue();
+            user.delete()
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(),"Account deleted",Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                                startActivityForResult(intent, 0);
+                            }
+                        }
+                    });
             return true;
         }
 
         if (id == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut();
+            FirebaseUser user =  FirebaseAuth.getInstance().getCurrentUser();
+            Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+            startActivityForResult(intent, 0);
             Toast.makeText(getApplicationContext(),"Log Out",Toast.LENGTH_LONG).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // your code.
+        FirebaseUser user =  FirebaseAuth.getInstance().getCurrentUser();
+        //user has signed out
+        if(user == null){
+            Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+            startActivityForResult(intent, 0);
+        }
     }
 
 
