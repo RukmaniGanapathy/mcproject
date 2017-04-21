f92580ba0bf19b2d7b40b3347d0b773faa9857b9
ayse <aozdemir2011@gmail.com>
2017-04-20 19:44:59 -0700

    grocery model added

---
+++ app/src/main/java/edu/project/mobilecomputing/mc_project/model/Grocery.java
@@ -0,0 +1,22 @@
+package edu.project.mobilecomputing.mc_project.model;
+
+public class Grocery {
+    private String userId;
+    private String items;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getItems() {
+        return items;
+    }
+
+    public void setItems(String items) {
+        this.items = items;
+    }
+}
